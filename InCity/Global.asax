fe9d8e8b6878705ec52488b157d274c6da5c0a86
terdened <terdened@mail.ru>
2014-09-28 10:31:27 +0200

    + Hello World

---
+++ InCity/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="InCity.MvcApplication" Language="C#" %>
---
+++ InCity/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(InCity.Startup))]
+namespace InCity
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
